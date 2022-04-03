e11cbde9ff2191097b517c6a3b562c7885d0bc23
Dennis Decoene <dennis.decoene@gmail.com>
2022-04-03 21:35:04 +0200

    default adaptations

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Ex-Professional Python, Javascript Dev. Now, open source projects, when I like, for fun. Crypto projects since 2013. Currently: Solidity and Next.js</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
